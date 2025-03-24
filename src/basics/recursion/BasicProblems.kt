045ae6fa2a00aa33b7e1ed62dbf08d3317928a4d
Saket37 <anandsaket37@gmail.com>
2025-03-24 23:54:27 +0530

    print n to 1

--- src/basics/recursion/BasicProblems.kt
+++ src/basics/recursion/BasicProblems.kt
@@ -1,8 +1,32 @@
 package basics.recursion
 
 fun main() {
-    readInputForPrintName()
+    //readInputForPrintName()
+    readInputToPrintFromTo1()
 }
+
+// print from n to 1
+private fun readInputToPrintFromTo1() {
+    print("Enter a number for which you want to print: ")
+    val num = readln().toInt()
+    //printNumbers(num, 1)
+    printNumber(num, num)
+}
+
+private fun printNumbers(n: Int, i: Int) {
+    if (n < 1) return
+    printNumbers(n - 1, i)
+    println(n)
+}
+
+// or we can do it like this
+private fun printNumber(i: Int, n: Int) {
+    if (i < 1) return
+    println(i)
+    printNumber(i - 1, n)
+
+}
+
 
 private fun readInputForPrintName() {
     print("Enter a number for which you want to print the names: ")
@@ -12,6 +36,7 @@ private fun readInputForPrintName() {
 
 private fun printName(i: Int, n: Int) {
     if (i > n) return
-    println("Saket")
     printName(i + 1, n)
+    println("Saket")
+
 }
