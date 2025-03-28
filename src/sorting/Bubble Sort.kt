9798a7a465e77205895626ea276218d38e98b504
Saket37 <anandsaket37@gmail.com>
2025-03-29 00:32:36 +0530

    bubble sort with best case

--- src/sorting/Bubble Sort.kt
+++ src/sorting/Bubble Sort.kt
@@ -5,15 +5,20 @@ fun main() {
     bubbleSort(arr)
     println(arr)
 }
+
 // TC - O(n^2)
+// Best case - O(n)
 private fun bubbleSort(arr: MutableList<Int>) {
     for (i in arr.size - 1 downTo 1) {
+        var didSwap = 0
         for (j in 0 until i) {
             if (arr[j] > arr[j + 1]) {
                 val temp = arr[j + 1]
                 arr[j + 1] = arr[j]
                 arr[j] = temp
+                didSwap = 1
             }
         }
+        if (didSwap == 0) break
     }
 }
