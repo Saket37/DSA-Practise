1b495f46c406b3ea8c445c5b8806a6fadea0c64e
Saket37 <anandsaket37@gmail.com>
2025-03-30 02:31:38 +0530

    quick sort

---
+++ src/sorting/Quick Sort.kt
@@ -0,0 +1,40 @@
+package sorting
+
+fun main() {
+    val arr = mutableListOf(13, 24, 78, 6, 1, 74, 34)
+    qs(arr, 0, arr.lastIndex)
+    println(arr)
+}
+
+private fun qs(arr: MutableList<Int>, low: Int, high: Int) {
+    if (low < high) {
+        val pivot = sort(arr, low, high)
+        qs(arr, low, pivot - 1)
+        qs(arr, pivot + 1, high)
+    }
+
+}
+
+private fun sort(arr: MutableList<Int>, low: Int, high: Int): Int {
+    val pivot = arr[low]
+    var i = low
+    var j = high
+
+    while (i < j) {
+        while (arr[i] <= pivot && i <= high - 1) {
+            i++
+        }
+        while (arr[j] > pivot && j >= low + 1) {
+            j--
+        }
+        if (i < j) {
+            val temp = arr[j]
+            arr[j] = arr[i]
+            arr[i] = temp
+        }
+        val temp = arr[low]
+        arr[low] = arr[j]
+        arr[j] = temp
+    }
+    return j
+}
